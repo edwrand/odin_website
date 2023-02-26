d14dfd89678ac5282935538be1620d1cd0a169f1
edwrand <ebbyrandall@yahoo.com>
2023-02-26 16:30:57 -0600

    added JS testing page for future practice

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ console.log(lastLetter("hello"))
 
 // fizbuzz
 
-let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+//let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
 for (let i = 1; i <= answer; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
